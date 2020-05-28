7162450eee415fe1d16065ff44d0633a17bf2bbd
Mackenzie Bruns <mackenziebruns503@gmail.com>
2020-05-28 16:32:02 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,7 @@
+var meals= { breakfast: "oatmeal"};
+var meals= new Object ({breakfast: 'oatmeal'});
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
+  obj[key]=value;
+  return obj
+}
+const recipe = {eggs:3}
